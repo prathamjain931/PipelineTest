98e32311b5be1d22fdcce0b707efb18fe5ba8cca
prathamjain931 <59327773+prathamjain931@users.noreply.github.com>
2022-08-04 12:31:29 +0530

    src uploaded

---
+++ src/main/java/rio/TestFirst.java
@@ -0,0 +1,10 @@
+package rio;
+
+public class TestFirst {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing DevOps Pipeline");
+	}
+
+}
